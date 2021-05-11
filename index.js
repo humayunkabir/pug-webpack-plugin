e506f0cf0e3aff4b2e176a3380b02766e8121529
Humayun kabir <humayunkabir.cep@gmail.com>
2021-05-11 17:18:36 +0600

    WIP

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 const fs = require('fs');
 const path = require('path');
 const pug = require('pug');
-
+const through = require('through2');
 class PugWebpackPlugin {
   constructor(config) {
     this.config = config;
   }
   apply(compiler) {
+    const opts = Object.assign({}, this.config.options);
+
+    // return through.obj(this.compilePug);
+
     const stream = fs.readFileSync(this.config.template);
     const render = pug.compile(stream, this.config.options);
     const html = render(this.config.locals);
     if (!fs.existsSync(this.config.path)) {
       fs.mkdirSync(this.config.path);
     }
+    this.write(html)
+  }
+
+  write(html) {
     fs.writeFile(
       path.resolve(this.config.path + '/' + this.config.options.filename),
       html,
@@ -23,6 +31,39 @@ class PugWebpackPlugin {
         }
       }
     );
+  }
+
+  compilePug(file, enc, cb) {
+    const data = Object.assign({}, opts.data, file.data || {});
+
+    opts.filename = file.path;
+    file.path = ext(file.path, opts.client ? '.js' : '.html');
+
+
+    if (file.isStream()) {
+      return cb(new Error('Streaming not supported'));
+    }
+
+    if (file.isBuffer()) {
+      try {
+        let compiled;
+        const contents = String(file.contents);
+        if (opts.verbose === true) {
+          console.log('compiling file', file.path);
+        }
+        if (opts.client) {
+          compiled = pug.compileClient(contents, opts);
+        } else {
+          compiled = pug.compile(contents, opts)(data);
+        }
+        console.log(compiled);
+        this.write(compiled);
+        file.contents = new Buffer(compiled);
+      } catch (e) {
+        return cb(new Error(e));
+      }
+    }
+    cb(null, file);
   }
 }
 
